6ce2f4b811be7de901c7a495c1d6ab2d747ac1e6
Gustavo Gonzalez <gustavo.gonzalez@correo.ucu.edu.uy>
2024-07-26 20:08:48 -0300

    servicesTestsV0.3

--- src/services/services_test/industryService.test2.js
+++ src/services/services_test/industryService.test2.js
@@ -1,7 +1,5 @@
-
 const industryService = require('../industryService');
-const IndustryModel = require('../models/industryModel');
-
+//const IndustryModel = require('../models/industryModel');
 describe('industryService', () => {
     // Verifica que getAll devuelva todas las industrias.
     describe('getAll', () => {
@@ -11,32 +9,25 @@ describe('industryService', () => {
             { id: 1, name: 'Technology' },
             { id: 2, name: 'Healthcare' }
         ];
-        jest.spyOn(IndustryModel, 'find').mockResolvedValue(mockIndustries);
-
+        jest.spyOn(industryService, 'getAll').mockResolvedValue(mockIndustries);
         // Act
         const result = await industryService.getAll();
-
         // Assert
         expect(result).toEqual(mockIndustries);
         });
-
         // Verifica que getAll devuelva un array vacío si no hay industrias.
         it('should return an empty array if no industries are found', async () => {
         // Arrange
-        jest.spyOn(IndustryModel, 'find').mockResolvedValue([]);
-
+        jest.spyOn(industryService, 'getAll').mockResolvedValue([]);
         // Act
         const result = await industryService.getAll();
-
         // Assert
         expect(result).toEqual([]);
         });
-
         // Verifica que getAll lance un error si hay un problema al recuperar las industrias.
         it('should throw an error if there is a problem retrieving industries', async () => {
         // Arrange
-        jest.spyOn(IndustryModel, 'find').mockRejectedValue(new Error('Database error'));
-
+        jest.spyOn(industryService, 'getAll').mockRejectedValue(new Error('Database error'));
         // Act & Assert
         await expect(industryService.getAll()).rejects.toThrow('Database error');
         });
