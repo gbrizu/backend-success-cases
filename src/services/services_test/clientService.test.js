895b1516f80bd80131a754ad4023373c9c95b7ef
Enzuii <enzuii@hotmail.com>
2024-07-26 21:02:18 -0300

    tests changes

--- src/services/services_test/clientService.test.js
+++ src/services/services_test/clientService.test.js
@@ -1,18 +1,14 @@
 const clientService = require('../clientService');
+const clientModel = require('../../models/clientModel.js');
 
 describe('clientService', () => {
     // Verifica que getAllClient devuelva todos los clientes.
     describe('getByIdClient', () => {
         it('should return the client for a given ID', async () => {
             // Arrange
-            const mockId = 1;
             const mockClient = { id: 1, name: 'John', surname: 'Doe', email: 'john.doe@example.com' };
-            jest.spyOn(clientService, 'getByIdClient').mockResolvedValue(mockClient);
-
-            // Act
-            const result = await clientService.getByIdClient(mockId);
-
-            // Assert
+            jest.spyOn(clientModel, 'findByPk').mockResolvedValue(mockClient);
+            const result = await clientService.getByIdClient();
             expect(result).toEqual(mockClient);
         });
 
@@ -20,8 +16,8 @@ describe('clientService', () => {
 
         it('should throw an error if the ID does not exist', async () => {
             // Arrange
-            const mockId = 1;
-            jest.spyOn(clientService, 'getByIdClient').mockResolvedValue(null);
+            const mockId = 1000;
+            jest.spyOn(clientModel, 'findByPk').mockResolvedValue(mockId);
 
             // Act & Assert
             await expect(clientService.getByIdClient(mockId)).rejects.toThrow('Client not found');
